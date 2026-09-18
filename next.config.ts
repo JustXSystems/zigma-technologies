d5cd1d44aa398325195e3b5571a31a73724f5167
Santhosh HS <iamsanthosh@gmail.com>
2026-09-18 12:46:42 +0530

    #17 CSP stylesheet block and the startTime error

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const csp = [
   "default-src 'self'",
   "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://plausible.io https://challenges.cloudflare.com",
-  "style-src 'self' 'unsafe-inline'",
+  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "img-src 'self' data: blob: https:",
-  "font-src 'self' data:",
+  "font-src 'self' data: https://fonts.gstatic.com",
   "connect-src 'self' https://www.google-analytics.com https://region1.google-analytics.com https://plausible.io https://challenges.cloudflare.com",
   "frame-src 'self' https://www.google.com https://maps.google.com https://challenges.cloudflare.com https://www.youtube.com https://player.vimeo.com",
   "media-src 'self' blob: https:",
